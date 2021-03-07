c0a1160d977c9ba2961467bf24dbea58885d5778
Martin <martno15@gmail.com>
2021-03-07 23:17:59 +0100

    completed the first 8 unit tests

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -4,6 +4,13 @@ export const isEmpty = (stringArrayOrObject) => {
   if (type === 'string') {
     return stringArrayOrObject === ''
   }
+  //checks if it is an array
+  else if (stringArrayOrObject.constructor.toString().indexOf("Array") !== -1) { //if you cannot find the index of "Array", it isn't an array
+    return stringArrayOrObject.length < 1
+  }
+  else if (type === 'object') {
+    return JSON.stringify(stringArrayOrObject)==JSON.stringify({}) //if turning an object into a string using JSON equates to doing the same with an empty object, then the object must be empty
+  }
 
   return false
 }
